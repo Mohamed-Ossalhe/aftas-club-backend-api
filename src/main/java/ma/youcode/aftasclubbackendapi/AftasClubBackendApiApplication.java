e4172ecfc0040b04b902ed043669137e26f27430
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-11 09:46:47 +0100

    init spring boot project

---
+++ src/main/java/ma/youcode/aftasclubbackendapi/AftasClubBackendApiApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.aftasclubbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasClubBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasClubBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youcode/aftasclubbackendapi/AftasClubBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.aftasclubbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasClubBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
