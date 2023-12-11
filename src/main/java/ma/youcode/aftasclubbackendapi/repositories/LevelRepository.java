e1793331e820ab416c2b38d2cc993a1fde1b0e4a
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-11 19:16:57 +0100

    add level repository

---
+++ src/main/java/ma/youcode/aftasclubbackendapi/repositories/LevelRepository.java
@@ -0,0 +1,7 @@
+package ma.youcode.aftasclubbackendapi.repositories;
+
+import ma.youcode.aftasclubbackendapi.entities.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
+}
