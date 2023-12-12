32a279c4a95aebe297efd4d6ae9080dfbcfa584b
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-12 18:41:47 +0100

    specify the entity, dto and id type for the Icrud interface in all services interfaces

--- src/main/java/ma/youcode/aftasclubbackendapi/services/FishService.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/services/FishService.java
@@ -1,8 +1,10 @@
 package ma.youcode.aftasclubbackendapi.services;
 
+import ma.youcode.aftasclubbackendapi.dto.FishDto;
+import ma.youcode.aftasclubbackendapi.entities.Fish;
 import ma.youcode.aftasclubbackendapi.interfaces.ICrud;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface FishService extends ICrud {
+public interface FishService extends ICrud<Fish, FishDto, String> {
 }
--- src/main/java/ma/youcode/aftasclubbackendapi/services/LevelService.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/services/LevelService.java
@@ -1,8 +1,10 @@
 package ma.youcode.aftasclubbackendapi.services;
 
+import ma.youcode.aftasclubbackendapi.dto.LevelDto;
+import ma.youcode.aftasclubbackendapi.entities.Level;
 import ma.youcode.aftasclubbackendapi.interfaces.ICrud;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface LevelService extends ICrud {
+public interface LevelService extends ICrud<Level, LevelDto, Integer> {
 }
