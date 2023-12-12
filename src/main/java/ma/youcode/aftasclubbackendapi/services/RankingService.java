e32838e7d88147870f5d0e01f40efabd432975e3
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-12 18:54:38 +0100

    edit ranking service

--- src/main/java/ma/youcode/aftasclubbackendapi/services/RankingService.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/services/RankingService.java
@@ -1,8 +1,11 @@
 package ma.youcode.aftasclubbackendapi.services;
 
-import ma.youcode.aftasclubbackendapi.interfaces.ICrud;
+import ma.youcode.aftasclubbackendapi.entities.Ranking;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public interface RankingService extends ICrud {
+public interface RankingService {
+    public Optional<Ranking> save(Ranking rank);
 }
