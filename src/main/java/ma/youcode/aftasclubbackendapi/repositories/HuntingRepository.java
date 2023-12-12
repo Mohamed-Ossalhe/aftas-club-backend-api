e967726e53d3d06987702d40291058bc7fb6fb70
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-12 10:04:37 +0100

    add @Repository annotation to repositories

--- src/main/java/ma/youcode/aftasclubbackendapi/repositories/HuntingRepository.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/repositories/HuntingRepository.java
@@ -2,6 +2,8 @@ package ma.youcode.aftasclubbackendapi.repositories;
 
 import ma.youcode.aftasclubbackendapi.entities.Hunting;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HuntingRepository extends JpaRepository<Hunting, Long> {
 }
--- src/main/java/ma/youcode/aftasclubbackendapi/repositories/RankingRepository.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/repositories/RankingRepository.java
@@ -2,6 +2,8 @@ package ma.youcode.aftasclubbackendapi.repositories;
 
 import ma.youcode.aftasclubbackendapi.entities.Ranking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RankingRepository extends JpaRepository<Ranking, Long> {
 }
