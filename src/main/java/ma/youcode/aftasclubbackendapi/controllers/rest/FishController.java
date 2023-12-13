37f91d7d0feaaccac3c2cded1602d90c8d02017f
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-13 16:09:32 +0100

    add fish service impl + impl controller adding crud functionalities + edit fish request object

--- src/main/java/ma/youcode/aftasclubbackendapi/controllers/rest/FishController.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/controllers/rest/FishController.java
@@ -1,47 +1,70 @@
 package ma.youcode.aftasclubbackendapi.controllers.rest;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import ma.youcode.aftasclubbackendapi.controllers.IController;
 import ma.youcode.aftasclubbackendapi.dto.FishDto;
 import ma.youcode.aftasclubbackendapi.dto.requests.FishRequest;
+import ma.youcode.aftasclubbackendapi.services.FishService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/fish")
+@RequiredArgsConstructor
 public class FishController implements IController<FishRequest, FishDto, String> {
+
+    private final FishService fishService;
     @Override
-    public ResponseEntity<FishDto> get(String s) {
-        return null;
+    @GetMapping("/{name}")
+    public ResponseEntity<FishDto> get(@PathVariable String name) {
+        Optional<FishDto> fishDto = fishService.find(name);
+        assert fishDto.isPresent();
+        return new ResponseEntity<>(fishDto.get(), HttpStatus.OK);
     }
 
     @Override
+    @GetMapping("/all")
     public ResponseEntity<List<FishDto>> getAll() {
-        return null;
+        return new ResponseEntity<>(fishService.getAll(), HttpStatus.OK);
     }
 
     @Override
+    @GetMapping
     public ResponseEntity<Page<FishDto>> getAllPaginated(Pageable pageable) {
-        return null;
+        return new ResponseEntity<>(fishService.getAll(pageable), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<FishDto> create(FishRequest fishRequest) {
-        return null;
+    @PostMapping("/create")
+    public ResponseEntity<FishDto> create(@Valid @RequestBody FishRequest fishRequest) {
+        Optional<FishDto> fishDto = fishService.create(fishRequest);
+        assert fishDto.isPresent();
+        return new ResponseEntity<>(fishDto.get(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<FishDto> update(FishRequest fishRequest, String s) {
-        return null;
+    @PatchMapping("/{name}/update")
+    public ResponseEntity<FishDto> update(@Valid @RequestBody FishRequest fishRequest, @PathVariable String name) {
+        Optional<FishDto> fishDto = fishService.update(fishRequest, name);
+        assert fishDto.isPresent();
+        return new ResponseEntity<>(fishDto.get(), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Map<String, String>> destroy(String s) {
-        return null;
+    @DeleteMapping("/{name}/delete")
+    public ResponseEntity<Map<String, String>> destroy(@PathVariable String name) {
+        Map<String, String> messages = new HashMap<>();
+        if (fishService.destroy(name)) messages.put("message", "Competition Deleted Successfully");
+        else messages.put("message", "Couldn't Delete Competition");
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 }
---
+++ src/main/java/ma/youcode/aftasclubbackendapi/services/implementation/FishServiceImpl.java
@@ -0,0 +1,81 @@
+package ma.youcode.aftasclubbackendapi.services.implementation;
+
+import lombok.RequiredArgsConstructor;
+import ma.youcode.aftasclubbackendapi.dto.FishDto;
+import ma.youcode.aftasclubbackendapi.dto.requests.FishRequest;
+import ma.youcode.aftasclubbackendapi.entities.Fish;
+import ma.youcode.aftasclubbackendapi.exceptions.AlreadyExistExceptions.FishAlreadyExistException;
+import ma.youcode.aftasclubbackendapi.exceptions.NotFoundExceptions.FishNotFoundException;
+import ma.youcode.aftasclubbackendapi.repositories.FishRepository;
+import ma.youcode.aftasclubbackendapi.services.FishService;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FishServiceImpl implements FishService {
+
+    private final FishRepository fishRepository;
+    private final ModelMapper mapper;
+
+    @Override
+    public List<FishDto> getAll() {
+        List<Fish> fishList = fishRepository.findAll();
+        if (fishList.isEmpty())
+            throw new FishNotFoundException("No Fish Found");
+        return fishList.stream().map(fish -> mapper.map(fish, FishDto.class)).toList();
+    }
+
+    @Override
+    public Page<FishDto> getAll(Pageable pageable) {
+        Page<Fish> fishPage = fishRepository.findAll(pageable);
+        if (fishPage.isEmpty())
+            throw new FishNotFoundException("No Fish Found");
+        return fishPage.map(fish -> mapper.map(fish, FishDto.class));
+    }
+
+    @Override
+    public Optional<FishDto> find(String s) {
+        Optional<Fish> fish = fishRepository.findById(s);
+        if (fish.isEmpty())
+            throw new FishNotFoundException("Fish Not Found With Name: " + s);
+        return Optional.of(mapper.map(fish, FishDto.class));
+    }
+
+    @Override
+    public Optional<FishDto> create(FishRequest fishRequest) {
+//        if (fishRepository.findById(fishRequest.getName()).isPresent())
+//            throw new FishAlreadyExistException("Fish already Exist with Name: " + fishRequest.getName());
+//        else {
+//            Fish fish = mapper.map(fishRequest, Fish.class);
+//            Fish savedFish = fishRepository.save(fish);
+//            return Optional.of(mapper.map(savedFish, FishDto.class));
+//        }
+        return Optional.of(mapper.map(fishRequest, FishDto.class));
+    }
+
+    @Override
+    public Optional<FishDto> update(FishRequest fishRequest, String name) {
+        if (fishRepository.findById(name).isEmpty())
+            throw new FishNotFoundException("Fish Not Found With Name: " + name);
+        else {
+            Fish fish = mapper.map(fishRequest, Fish.class);
+            Fish savedFish = fishRepository.save(fish);
+            return Optional.of(mapper.map(savedFish, FishDto.class));
+        }
+    }
+
+    @Override
+    public boolean destroy(String name) {
+        Optional<Fish> fish = fishRepository.findById(name);
+        if (fish.isPresent()){
+            fishRepository.delete(fish.get());
+            return true;
+        }else throw new FishNotFoundException("Fish Not Found With Name: " + name);
+    }
+}
