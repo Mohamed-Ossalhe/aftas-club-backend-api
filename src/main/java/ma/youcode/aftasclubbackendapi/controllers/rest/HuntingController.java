e5f268322621dbe762ef52f3e71867159b6e0dac
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-12-17 13:37:18 +0100

    implement all service methods in hunting controller and add methods routes

--- src/main/java/ma/youcode/aftasclubbackendapi/controllers/rest/HuntingController.java
+++ src/main/java/ma/youcode/aftasclubbackendapi/controllers/rest/HuntingController.java
@@ -1,49 +1,71 @@
 package ma.youcode.aftasclubbackendapi.controllers.rest;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import ma.youcode.aftasclubbackendapi.controllers.IController;
 import ma.youcode.aftasclubbackendapi.dto.HuntingDto;
 import ma.youcode.aftasclubbackendapi.dto.requests.HuntingRequest;
+import ma.youcode.aftasclubbackendapi.services.HuntingService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/hunts")
+@RequiredArgsConstructor
 @CrossOrigin("*") // TODO: this should be the frontend url only
 public class HuntingController implements IController<HuntingRequest, HuntingDto, Integer> {
+
+    private final HuntingService huntingService;
     @Override
-    public ResponseEntity<HuntingDto> get(Integer integer) {
-        return null;
+    @GetMapping("/{id}")
+    public ResponseEntity<HuntingDto> get(@PathVariable Integer id) {
+        Optional<HuntingDto> hunt = huntingService.find(id);
+        assert hunt.isPresent();
+        return new ResponseEntity<>(hunt.get(), HttpStatus.FOUND);
     }
 
     @Override
+    @GetMapping("/all")
     public ResponseEntity<List<HuntingDto>> getAll() {
-        return null;
+        return new ResponseEntity<>(huntingService.getAll(), HttpStatus.OK);
     }
 
     @Override
+    @GetMapping
     public ResponseEntity<Page<HuntingDto>> getAllPaginated(Pageable pageable) {
-        return null;
+        return new ResponseEntity<>(huntingService.getAll(pageable), HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<HuntingDto> create(HuntingRequest huntingRequest) {
-        return null;
+    @PostMapping("/create")
+    public ResponseEntity<HuntingDto> create(@Valid @RequestBody HuntingRequest huntingRequest) {
+        Optional<HuntingDto> savedHunt = huntingService.create(huntingRequest);
+        assert savedHunt.isPresent();
+        return new ResponseEntity<>(savedHunt.get(), HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<HuntingDto> update(HuntingRequest huntingRequest, Integer integer) {
-        return null;
+    @PatchMapping("/{id}/update")
+    public ResponseEntity<HuntingDto> update(@Valid @RequestBody HuntingRequest huntingRequest, @PathVariable Integer id) {
+        Optional<HuntingDto> updatedHunt = huntingService.update(huntingRequest, id);
+        assert updatedHunt.isPresent();
+        return new ResponseEntity<>(updatedHunt.get(), HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<Map<String, String>> destroy(Integer integer) {
-        return null;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> destroy(@PathVariable Integer id) {
+        Map<String, String> messages = new HashMap<>();
+        if (huntingService.destroy(id)) messages.put("message", "Hunt Deleted Successfully");
+        else messages.put("message", "Hunt couldn't be Deleted");
+        return new ResponseEntity<>(messages, HttpStatus.OK);
     }
 }
